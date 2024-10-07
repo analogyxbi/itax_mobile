9a07a98af2388f873293497006f7e8dbd2cee56f
Satendra Kumar R <satendra@analogyx.com>
2024-10-07 11:56:56 +0530

    unlock-all-freatures

--- App.js
+++ App.js
@@ -127,8 +127,8 @@ const MainStack = ({ isAuthenticated, setIsAuthenticated }) => {
       <Stack.Screen name="cycle_details" component={CycleApp} />
       <Stack.Screen name="Help" component={HelpScreen} />
       <Stack.Screen name="UserInfo" component={UserInfo} />
-      {/* <Stack.Screen name="quantity_adjustments" component={QuantityAdjustments} />
-      <Stack.Screen name="miscellaneous_material" component={MiscellaneousMaterial} /> */}
+      <Stack.Screen name="quantity_adjustments" component={QuantityAdjustments} />
+      <Stack.Screen name="miscellaneous_material" component={MiscellaneousMaterial} />
       <Stack.Screen name="PrivacyPolicy" component={PrivacyPolicy} />
       <Stack.Screen name="add_part_to_cycle" component={AddPartToCycle} />
 
